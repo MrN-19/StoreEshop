dd8c4cd8c8d3a199d017e52abd8c1743edbd36f5
unknown <mohammadreze.mrn.29@gmail.com>
2022-04-28 22:13:55 +0430

    add static files and media files configs

--- eshop/settings.py
+++ eshop/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'eshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,9 +118,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+import os
 
-STATIC_URL = 'static/'
+STATIC_URL = 'assets/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,"assets")
+]
 
+MEDIA_ROOT = BASE_DIR / "media/"
+MEDIA_URL = "media/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
