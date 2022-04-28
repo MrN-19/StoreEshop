64dd0b63ff196774da6fbd93cf8f1a909fc53584
unknown <mohammadreze.mrn.29@gmail.com>
2022-04-28 23:39:38 +0430

    create money currency modelin setting app

--- setting/admin.py
+++ setting/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import AppSetting,MoneyCurrency
 
-# Register your models here.
+admin.site.register(AppSetting)
+admin.site.register(MoneyCurrency)
--- setting/models.py
+++ setting/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class AppSetting(models.Model):
+    name = models.CharField(max_length=200,verbose_name="نام سایت")
+    logo = models.ImageField(verbose_name="لوگو",upload_to = "Setting/Logo")
+    contact_info = models.CharField(max_length=50,verbose_name="پل ارتباطی",null=True)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = "تنظیمات برنامه"
+        verbose_name_plural = "تنظیمات برنامه"
+    
+class MoneyCurrency(models.Model):
+    CURRENCY = (
+        ("toman","تومان"),("rial","ریال"),("dolor","دلار"),("euro","یورو")
+    )
+    currency = models.CharField(max_length=200,verbose_name="واحد پول",choices=CURRENCY)
+
+    def __str__(self):
+        return self.currency
+    class Meta:
+        verbose_name = "واحد پول"
+        verbose_name_plural = "واحد پول"
+    
