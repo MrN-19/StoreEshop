1f7ea467ae6c32665ccc73a80e541a5f9e9fa8aa
unknown <mohammadreze.mrn.29@gmail.com>
2022-04-29 00:12:32 +0430

    add views to main app in application

---
+++ eshop/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from product.models import BuyCount, Product,ProductCategory
+def home(request):
+    all_product_recent = Product.objects.order_by("-publish_date").filter(is_active = True).all()[:6]
+    categories_header = ProductCategory.objects.filter(is_header = True)
+    categories = ProductCategory.objects.all()
+    highest_products_bought = BuyCount.objects.order_by("-count").all()
+    context = {
+        "products" : all_product_recent,
+        "categories" : categories,
+        "categories_header" : categories_header,
+        "highest_products_bought" : highest_products_bought
+    }
+    return render(request,"home.html",context)
+
+
