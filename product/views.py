34d5e24585e084e6a4160406c78b271d8de1e5b6
unknown <mohammadreze.mrn.29@gmail.com>
2022-05-01 12:29:29 +0430

    add get products by category view

--- product/views.py
+++ product/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,get_list_or_404
+from .models import ProductCategory,Product
 
-def get_product_by_category(request):
-    pass
+
+def get_product_by_category(request,slug):
+    category = get_object_or_404(ProductCategory,slug = slug)
+    products = get_list_or_404(Product,category = category)
+    context = {
+        "products" : products
+    }
+    return render(request,"products/products.html",context)
+
