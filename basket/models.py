0ce5b338d6fd08490448307665b14bd56f804686
unknown <mohammadreze.mrn.29@gmail.com>
2022-06-18 23:41:04 +0430

    add Basket model in basket app

--- basket/models.py
+++ basket/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from product.models import Product
+class Basket(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,verbose_name="کاربر")
+    product = models.ForeignKey(Product,on_delete=models.CASCADE,verbose_name="محصول")
+    added_date = models.DateTimeField(auto_now_add=True,verbose_name="تاریخ ثبت")
 
-# Create your models here.
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name = "سبد خرید"
+        verbose_name_plural = "سبد خرید"
