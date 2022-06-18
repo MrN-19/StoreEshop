397ee7068400c0604952ac446e9c6211e9cc7b7a
unknown <mohammadreze.mrn.29@gmail.com>
2022-06-18 21:50:08 +0430

    add set_comment path into urls

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import get_product_by_category,single_product,get_product_by_tags
+from .views import get_product_by_category,single_product,get_product_by_tags,set_comment
 app_name = "product"
 
 urlpatterns = [
     path("product-category/<slug>",get_product_by_category,name="getproductbycategory"),
     path("product/<slug>/<int:id>",single_product,name="singleproduct"),
     path("product/tags/<str:tag>",get_product_by_tags,name="getproductsbytag"),
+    path("product/comment",set_comment,name="setcomment"),
     
 ]
