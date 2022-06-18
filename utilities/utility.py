1fc7ce0d54f257e63cf0f1e53477fa015299f831
unknown <mohammadreze.mrn.29@gmail.com>
2022-06-18 21:52:48 +0430

    add iziToast css and js file and create messages.html file to showing status messages and declare theme into footerRefrence.html HeaderRefrence.html and base.html and other ....

--- utilities/utility.py
+++ utilities/utility.py
@@ -4,3 +4,9 @@ class Calculator:
         answer = 0
         answer = ((100 - percent) * price) / 100
         return answer
+    
+class StringModifier:
+    @classmethod
+    def change_request_path_value(cls,request,new_value):
+        request.path = new_value
+        return request
